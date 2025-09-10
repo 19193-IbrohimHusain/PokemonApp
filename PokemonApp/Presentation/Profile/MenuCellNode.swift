a3c62a42a9e90068cb9b7a046e8cf5aed124053e
19193-IbrohimHusain <84296114+19193-IbrohimHusain@users.noreply.github.com>
2025-09-10 18:45:13 +0700

    feat: add MenuCellNode for ProfileVC

---
+++ PokemonApp/Presentation/Profile/MenuCellNode.swift
@@ -0,0 +1,41 @@
+//
+//  MenuCellNode.swift
+//  PokemonApp
+//
+//  Created by Ibrohim Husain on 06/09/25.
+//
+
+import AsyncDisplayKit
+
+final class MenuCellNode: BaseCellNode {
+    private let iconNode = ASImageNode()
+    private let titleNode = ASTextNode()
+
+    init(title: String?, icon: UIImage?, tint: UIColor?) {
+        super.init()
+        selectionStyle = .none
+        accessoryType = .disclosureIndicator
+        backgroundColor = .systemBackground
+        iconNode.image = icon?.resized(to: icon?.size ?? .zero, tint: tint)
+        iconNode.style.preferredSize = CGSize(width: 22, height: 22)
+        titleNode.attributedText = NSAttributedString(
+            string: title.orEmpty,
+            attributes: [
+                .font: UIFont.systemFont(ofSize: 16),
+                .foregroundColor: UIColor.label
+            ]
+        )
+    }
+
+    override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+        let stack = ASStackLayoutSpec(
+            direction: .horizontal,
+            spacing: 12,
+            justifyContent: .start,
+            alignItems: .center,
+            children: [iconNode, titleNode]
+        )
+
+        return ASInsetLayoutSpec(insets: .init(top: 10, left: 16, bottom: 10, right: 16), child: stack)
+    }
+}

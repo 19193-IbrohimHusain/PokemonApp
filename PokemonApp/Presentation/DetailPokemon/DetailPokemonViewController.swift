3ca6d06bb7e1066d56c86712114f4884827ea870
19193-IbrohimHusain <84296114+19193-IbrohimHusain@users.noreply.github.com>
2025-09-10 18:56:04 +0700

    refactor: swap uikit component in DetailPokemonVC with texture

--- PokemonApp/Presentation/DetailPokemon/DetailPokemonViewController.swift
+++ PokemonApp/Presentation/DetailPokemon/DetailPokemonViewController.swift
@@ -5,14 +5,13 @@
 //  Created by Ibrohim Husain on 14/08/25.
 //
 
-import UIKit
-import SnapKit
+import AsyncDisplayKit
 
 enum DetailPokemonSection: Int, CaseIterable {
     case image, trivia, info
 }
 
-final class DetailPokemonViewController: BaseViewController {
+final class DetailPokemonViewController: BaseASDKViewController {
     private let favoriteIcon = UIBarButtonItem(
         image: SFSymbols.favorite,
         style: .plain,
@@ -20,41 +19,39 @@ final class DetailPokemonViewController: BaseViewController {
         action: nil
     )
     
-    private let tableView = UITableView().configure {
-        $0.separatorStyle = .none
+    private let tableNode = ASTableNode().configure {
         $0.backgroundColor = .systemBackground
-        $0.showsVerticalScrollIndicator = false
+        $0.view.separatorStyle = .none
+        $0.view.showsVerticalScrollIndicator = false
     }
     
     private let viewModel = DetailPokemonViewModel()
     
     private var isFavorite = false
     
+    override func configNode() {
+        node.backgroundColor = .systemBackground
+        tableNode.dataSource = self
+        node.layoutSpecBlock = { [weak self] _,_ in
+            guard let self = self else { return ASLayoutSpec() }
+            return ASInsetLayoutSpec(insets: .zero, child: tableNode)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupView()
+        setupNavigationBar()
         bindEvent()
         viewModel.fetchAdditionalDetail()
     }
     
-    private func setupView() {
+    private func setupNavigationBar() {
         favoriteIcon.tintColor = .systemRed
         isFavorite = viewModel.isPokemonFavorite()
         favoriteIcon.image = isFavorite ? SFSymbols.favoriteFilled : SFSymbols.favorite
         favoriteIcon.target = self
         favoriteIcon.action = #selector(addFavorite)
         navigationItem.rightBarButtonItem = favoriteIcon
-        view.backgroundColor = .systemBackground
-        view.addSubview(tableView)
-        tableView.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide)
-            $0.horizontalEdges.bottom.equalToSuperview()
-        }
-        tableView.dataSource = self
-        tableView.delegate = self
-        tableView.registerCell(DetailPokemonImageTableViewCell.self)
-        tableView.registerCell(DetailPokemonTriviaTableViewCell.self)
-        tableView.registerCell(DetailPokemonInfoTableViewCell.self)
     }
     
     private func bindEvent() {
@@ -64,12 +61,12 @@ final class DetailPokemonViewController: BaseViewController {
                 guard let self = self else { return }
                 switch $0 {
                 case .loading:
-                    LoadingHUD.show(in: self.view)
+                    LoadingHUDNode.show(in: self.node)
                 case .finished:
-                    LoadingHUD.hide(from: self.view)
-                    self.tableView.reloadData()
+                    LoadingHUDNode.hide()
+                    self.tableNode.reloadData()
                 default:
-                    LoadingHUD.hide(from: self.view)
+                    LoadingHUDNode.hide()
                 }
             }
             .store(in: &cancellables)
@@ -91,12 +88,12 @@ final class DetailPokemonViewController: BaseViewController {
     }
 }
 
-extension DetailPokemonViewController: UITableViewDataSource {
-    func numberOfSections(in tableView: UITableView) -> Int {
+extension DetailPokemonViewController: ASTableDataSource {
+    func numberOfSections(in tableNode: ASTableNode) -> Int {
         return DetailPokemonSection.allCases.count
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableNode(_ tableNode: ASTableNode, numberOfRowsInSection section: Int) -> Int {
         guard let section = DetailPokemonSection.allCases[safe: section] else { return 0 }
         switch section {
         case .image:
@@ -108,29 +105,19 @@ extension DetailPokemonViewController: UITableViewDataSource {
         }
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let section = DetailPokemonSection.allCases[safe: indexPath.section] else { return UITableViewCell() }
+    func tableNode(_ tableNode: ASTableNode, nodeBlockForRowAt indexPath: IndexPath) -> ASCellNodeBlock {
+        guard let section = DetailPokemonSection.allCases[safe: indexPath.section] else { return { .init() } }
         switch section {
         case .image:
-            let cell: DetailPokemonImageTableViewCell = tableView.dequeueReusableCell(forIndexPath: indexPath)
-            guard let data = viewModel.dataDetail else { return cell }
-            cell.setContent(with: data, genera: viewModel.pokemonGenera)
-            return cell
+            guard let data = viewModel.dataDetail else { return { .init() } }
+            let genera = viewModel.pokemonGenera
+            return { DetailPokemonImageCellNode(data: data, genera: genera) }
         case .trivia:
-            let cell: DetailPokemonTriviaTableViewCell = tableView.dequeueReusableCell(forIndexPath: indexPath)
-            cell.triviaLabel.text = viewModel.pokemonTrivia
-            return cell
+            let trivia = viewModel.pokemonTrivia
+            return { DetailPokemonTriviaCellNode(trivia) }
         case .info:
-            let cell: DetailPokemonInfoTableViewCell = tableView.dequeueReusableCell(forIndexPath: indexPath)
-            guard let data = viewModel.pokemonInfo[safe: indexPath.row] else { return cell }
-            cell.setContent(title: data.title, desc: data.desc, chipFormat: data.chipFormat)
-            return cell
+            guard let data = viewModel.pokemonInfo[safe: indexPath.row] else { return { .init() } }
+            return { DetailPokemonInfoCellNode(title: data.title, desc: data.desc, chipFormat: data.chipFormat) }
         }
-    }
-}
-
-extension DetailPokemonViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return UITableView.automaticDimension
     }
 }

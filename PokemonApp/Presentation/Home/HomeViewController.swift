e4d443987493c047844cd9f4c97cd44336f12f02
19193-IbrohimHusain <84296114+19193-IbrohimHusain@users.noreply.github.com>
2025-09-10 18:44:12 +0700

    refactor: swap uikit component in HomeVC with texture

--- PokemonApp/Presentation/Home/HomeViewController.swift
+++ PokemonApp/Presentation/Home/HomeViewController.swift
@@ -5,38 +5,37 @@
 //  Created by Ibrohim Husain on 14/08/25.
 //
 
-import UIKit
-import SnapKit
+import AsyncDisplayKit
 
-final class HomeViewController: BaseViewController {
-    private let tableView = UITableView().configure {
-        $0.separatorStyle = .none
-        $0.showsVerticalScrollIndicator = false
+final class HomeViewController: BaseASDKViewController {
+    private let tableNode = ASTableNode().configure {
         $0.backgroundColor = .systemGroupedBackground
         $0.contentInset = .init(top: 8, left: 0, bottom: 8, right: 0)
+        $0.view.separatorStyle = .none
+        $0.view.showsVerticalScrollIndicator = false
     }
     
     private let viewModel = HomeViewModel()
     
+    override func configNode() {
+        node.backgroundColor = .systemGroupedBackground
+        tableNode.dataSource = self
+        tableNode.delegate = self
+        node.layoutSpecBlock = { [weak self] _,_ in
+            guard let self = self else { return ASLayoutSpec() }
+            return ASInsetLayoutSpec(insets: .zero, child: tableNode)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupView()
         bindEvent()
         viewModel.fetchPokemonList()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        tableView.reloadData()
-    }
-    
-    private func setupView() {
-        view.backgroundColor = .systemGroupedBackground
-        view.addSubview(tableView)
-        tableView.snp.makeConstraints { $0.edges.equalToSuperview() }
-        tableView.dataSource = self
-        tableView.delegate = self
-        tableView.registerCell(PokemonCardTableViewCell.self)
+        tableNode.reloadData()
     }
     
     private func bindEvent() {
@@ -45,10 +44,10 @@ final class HomeViewController: BaseViewController {
             .sink { [weak self] in
                 guard let self = self else { return }
                 switch $0 {
-                case .loading where viewModel.pokemonList.isEmpty:
-                    LoadingHUD.show(in: self.view)
+                case .loading where self.viewModel.pokemonList.isEmpty:
+                    LoadingHUDNode.show()
                 default:
-                    LoadingHUD.hide(from: self.view)
+                    LoadingHUDNode.hide()
                 }
             }
             .store(in: &cancellables)
@@ -57,9 +56,8 @@ final class HomeViewController: BaseViewController {
             .receive(on: RunLoop.main)
             .sink { [weak self] indexPaths in
                 guard let self = self else { return }
-                guard !indexPaths.isEmpty else { return }
-                self.tableView.performBatchUpdates({
-                    self.tableView.insertRows(at: indexPaths, with: .automatic)
+                self.tableNode.performBatchUpdates({
+                    self.tableNode.insertRows(at: indexPaths, with: .automatic)
                 })
             }
             .store(in: &cancellables)
@@ -70,45 +68,39 @@ final class HomeViewController: BaseViewController {
         vc.setContent(with: pokemon)
         navigationController?.pushViewController(vc, animated: true)
     }
-    
-    internal func fetchPokemonList() -> [PokemonDetailModel] {
-        return viewModel.pokemonList
-    }
 }
 
-extension HomeViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+extension HomeViewController: ASTableDataSource {
+    func tableNode(_ tableNode: ASTableNode, numberOfRowsInSection section: Int) -> Int {
         return viewModel.pokemonList.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: PokemonCardTableViewCell = tableView.dequeueReusableCell(forIndexPath: indexPath)
-        guard let data = viewModel.pokemonList[safe: indexPath.row] else { return cell }
-        cell.setContent(with: data, isFavorite: viewModel.isPokemonFavorite(data))
-        cell.toggleFavorite = { [weak self] save in
-            guard let self = self else { return }
-            if save {
-                self.viewModel.saveFavoritePokemon(data)
-            } else {
-                self.viewModel.deleteFavoritePokemon(data)
+    func tableNode(_ tableNode: ASTableNode, nodeBlockForRowAt indexPath: IndexPath) -> ASCellNodeBlock {
+        guard let data = viewModel.pokemonList[safe: indexPath.row] else { return { .init() } }
+        let isFav = viewModel.isPokemonFavorite(data)
+        return {
+            let cell = PokemonCardCellNode(data: data, isFavorite: isFav)
+            cell.toggleFavorite = { [weak self] save in
+                guard let self = self else { return }
+                if save {
+                    self.viewModel.saveFavoritePokemon(data)
+                } else {
+                    self.viewModel.deleteFavoritePokemon(data)
+                }
+                tableNode.reloadRows(at: [indexPath], with: .automatic)
             }
-            tableView.reloadRows(at: [indexPath], with: .automatic)
+            return cell
         }
-        return cell
     }
 }
 
-extension HomeViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        guard indexPath.row == viewModel.pokemonList.count - 2 else { return }
+extension HomeViewController: ASTableDelegate {
+    func tableNode(_ tableNode: ASTableNode, willDisplayRowWith node: ASCellNode) {
+        guard node.indexPath?.row == viewModel.pokemonList.count - 1 else { return }
         viewModel.fetchPokemonList()
     }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return UITableView.automaticDimension
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func tableNode(_ tableNode: ASTableNode, didSelectRowAt indexPath: IndexPath) {
         guard let data = viewModel.pokemonList[safe: indexPath.row] else { return }
         navigateToDetailPokemon(with: data)
     }
